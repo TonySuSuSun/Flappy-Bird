2baf6625bfbc8afe058fe4d3d593c8481067c594
Sususun <charactor38.tony@gmail.com>
2025-04-12 00:04:05 +0800

    Initial Commit

---
+++ Flappy BirdTests/Flappy_BirdTests.swift
@@ -0,0 +1,17 @@
+//
+//  Flappy_BirdTests.swift
+//  Flappy BirdTests
+//
+//  Created by 蘇炫瑋 on 2025/4/12.
+//
+
+import Testing
+@testable import Flappy_Bird
+
+struct Flappy_BirdTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
