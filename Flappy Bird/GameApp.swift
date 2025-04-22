cf163f2fbf0b7af0d2ff6f2674349762857dca52
TonySuSuSun <charactor38.tony@gmail.com>
2025-04-22 21:40:47 +0800

    初始檔案

---
+++ Flappy Bird/GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameApp.swift
+//  Flappy Bird
+//
+//  Created by 蘇炫瑋 on 2025/4/12.
+//
+
+import SwiftUI
+
+@main
+struct GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            GameView()
+        }
+    }
+}
---
+++ Flappy Bird/GameView.swift
@@ -0,0 +1,23 @@
+//
+//  GameView.swift
+//  Flappy Bird
+//
+//  Created by 蘇炫瑋 on 2025/4/12.
+//
+
+/*
+import SpriteKit
+import SwiftUI
+
+struct GameView: View {
+    var scene: SKScene {
+        let scene = GameScene(size: CGSize(width: 1024, height: 768))
+        scene.scaleMode = .aspectFit
+        return scene
+    }
+    var body: some View {
+        SpriteView(scene: scene)
+            .ignoresSafeArea()
+    }
+}
+*/
