338d32a8cb0782d4f4a41488f89dea9f943f3a9a
TonySuSuSun <charactor38.tony@gmail.com>
2025-04-26 22:23:12 +0800

    Create Game Menu

--- Flappy Bird/GameScene.swift
+++ Flappy Bird/GameScene.swift
@@ -10,9 +10,67 @@ import SpriteKit
 import SwiftUI
 
 class GameScene: SKScene {
+    
+    var ground:SKSpriteNode!
+    var title:SKLabelNode!
+    var startButton:SKLabelNode!
+    var scoreButton:SKLabelNode!
 
+    func createGround() {
+        let ground = SKSpriteNode(imageNamed: "ground.png")
+        ground.size = CGSize(width: 1536, height: 768)
+        ground.position = CGPoint(x: 512, y: 384)
+        ground.zPosition = 0
+        ground.name = "ground"
+        addChild(ground)
+    }
+    
+    func createTitle(){
+        let title = SKLabelNode(fontNamed: "Impact")
+        title.text = "Flappy Bird"
+        title.fontColor = UIColor.black
+        title.horizontalAlignmentMode = .center
+        title.fontSize = 108
+        title.position = CGPoint(x: 512, y: 624)
+        title.zPosition = 1
+        addChild(title)
+    }
+    
+    func createStartButton(){
+        let startButton = SKLabelNode(fontNamed: "Impact")
+        startButton.text = "Start"
+        startButton.fontColor = UIColor.black
+        startButton.horizontalAlignmentMode = .center
+        startButton.fontSize = 72
+        startButton.position = CGPoint(x: 512, y: 384)
+        startButton.zPosition = 1
+        addChild(startButton)
+    }
+    
+    func createScoreButton(){
+        let scoreButton = SKLabelNode(fontNamed: "Impact")
+        scoreButton.text = "Score"
+        scoreButton.fontColor = UIColor.black
+        scoreButton.horizontalAlignmentMode = .center
+        scoreButton.fontSize = 72
+        scoreButton.position = CGPoint(x: 512, y: 276)
+        scoreButton.zPosition = 1
+        addChild(scoreButton)
+    }
+    
     override func didMove(to view: SKView) {
-
+        self.anchorPoint = CGPoint(x:0, y:0)
+        let sky = SKSpriteNode(imageNamed: "sky.png")
+        sky.size = CGSize(width: 1536, height: 768)
+        sky.position = CGPoint(x: 512, y: 384)
+        sky.blendMode = .replace
+        sky.zPosition = -1
+        addChild(sky)
+        
+        createGround()
+        createTitle()
+        createStartButton()
+        createScoreButton()
     }
 
     func touchDown(atPoint pos: CGPoint) {
