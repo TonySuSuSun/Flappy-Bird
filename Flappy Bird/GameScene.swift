d43650e8b7e2c23ab70dedcdc69b7a85c595e465
TonySuSuSun <charactor38.tony@gmail.com>
2025-04-30 16:51:18 +0800

    Add bird and ceiling

--- Flappy Bird/GameScene.swift
+++ Flappy Bird/GameScene.swift
@@ -11,24 +11,52 @@ import SwiftUI
 
 class GameScene: SKScene {
     
+    var ceiling:SKSpriteNode!
     var ground:SKSpriteNode!
+    var bird:SKSpriteNode!
+    
+    var birdTextureAtlas = SKTextureAtlas()
+    var birdTextureArray = [SKTexture]()
+    
     var title:SKLabelNode!
     var startButton:SKLabelNode!
     var scoreButton:SKLabelNode!
 
+    var gameScore: SKLabelNode!
+    var score = 0 {
+        didSet {
+            gameScore.text = "\(score)"
+        }
+    }
+    
     var isGameStart = false
     
+    func createCeiling(){
+        ceiling = SKSpriteNode()
+        ceiling.size = CGSize(width: 1534, height: 104)
+        ceiling.position = CGPoint(x: 384, y: 1140)
+        ceiling.zPosition = 0
+        ceiling.name = "ceiling"
+        addChild(ceiling)
+        
+        ceiling.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 1534, height: 104))
+        ceiling.physicsBody?.affectedByGravity = false
+    }
+    
     func createGround() {
-        let ground = SKSpriteNode(imageNamed: "ground.png")
-        ground.size = CGSize(width: 1536, height: 768)
-        ground.position = CGPoint(x: 512, y: 384)
+        ground = SKSpriteNode(imageNamed: "ground.png")
+        ground.size = CGSize(width: 1534, height: 104)
+        ground.position = CGPoint(x: 384, y: 52)
         ground.zPosition = 0
         ground.name = "ground"
         addChild(ground)
+        
+        ground.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 1534, height: 104))
+        ground.physicsBody?.affectedByGravity = false
     }
     
     func createTitle(){
-        let title = SKLabelNode(fontNamed: "Impact")
+        title = SKLabelNode(fontNamed: "Impact")
         title.text = "Flappy Bird"
         title.fontColor = UIColor.black
         title.horizontalAlignmentMode = .center
@@ -39,25 +67,58 @@ class GameScene: SKScene {
     }
     
     func createStartButton(){
-        let startButton = SKLabelNode(fontNamed: "Impact")
+        startButton = SKLabelNode(fontNamed: "Impact")
         startButton.text = "Start"
         startButton.fontColor = UIColor.black
         startButton.horizontalAlignmentMode = .center
         startButton.fontSize = 72
         startButton.position = CGPoint(x: 384, y: 512)
         startButton.zPosition = 1
+        startButton.name = "Start"
         addChild(startButton)
     }
     
     func createScoreButton(){
-        let scoreButton = SKLabelNode(fontNamed: "Impact")
+        scoreButton = SKLabelNode(fontNamed: "Impact")
         scoreButton.text = "Score"
         scoreButton.fontColor = UIColor.black
         scoreButton.horizontalAlignmentMode = .center
         scoreButton.fontSize = 72
         scoreButton.position = CGPoint(x: 384, y: 372)
         scoreButton.zPosition = 1
+        scoreButton.name = "Score"
         addChild(scoreButton)
+    }
+    
+    func createScore(){
+        gameScore = SKLabelNode(fontNamed: "Impact")
+        gameScore.fontColor = UIColor.black
+        gameScore.horizontalAlignmentMode = .center
+        gameScore.fontSize = 72
+        gameScore.position = CGPoint(x: 384, y: 896)
+        gameScore.zPosition = 1
+        addChild(gameScore)
+        score = 0
+    }
+    
+    func createBird(){
+        birdTextureAtlas = SKTextureAtlas(named: "bird")
+        for i in 1...birdTextureAtlas.textureNames.count{
+            var name = "bird-\(i)"
+            birdTextureArray.append(SKTexture(imageNamed: name))
+        }
+        
+        bird = SKSpriteNode(imageNamed: birdTextureAtlas.textureNames[0])
+        bird.size = CGSize(width: 105, height: 90.75)
+        bird.position = CGPoint(x: 336, y: 512)
+        bird.zPosition = 0
+        bird.name = "bird"
+        addChild(bird)
+        
+        ground.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 105, height: 90.75))
+        ground.physicsBody?.affectedByGravity = false
+        
+        // bird.run(SKAction.repeatForever(SKAction.animate(withNormalTextures: birdTextureArray, timePerFrame: 0.2)))
     }
     
     override func didMove(to view: SKView) {
@@ -68,11 +129,14 @@ class GameScene: SKScene {
         sky.blendMode = .replace
         sky.zPosition = -1
         addChild(sky)
+        physicsWorld.gravity = CGVector(dx: 0, dy: -9.8)
         
+        createCeiling()
         createGround()
         createTitle()
         createStartButton()
         createScoreButton()
+        
     }
 
     func touchDown(atPoint pos: CGPoint) {
@@ -88,7 +152,23 @@ class GameScene: SKScene {
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        let nodesAtPoint = nodes(at: location)
+        for case let node as SKSpriteNode in nodesAtPoint {
+            if node.name == "Start" {
+                
+            } else if node.name == "Score" {
+                
+            } else if node.name == "OK" {
+                
+            } else if node.name == "Pause" {
+                
+            } else {
+                
+            }
+        }
+        
     }
 
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
