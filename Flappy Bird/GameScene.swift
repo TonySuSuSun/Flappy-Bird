5f101f506b89e3293f527ff6d9f031f6671d1c96
TonySuSuSun <charactor38.tony@gmail.com>
2025-05-07 15:25:40 +0800

    Add EndGame Scene, Restart Button & Back to Menu Button

--- Flappy Bird/GameScene.swift
+++ Flappy Bird/GameScene.swift
@@ -26,6 +26,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var hintTitle: SKLabelNode!  // 提示點擊開始遊戲的字(Tab to Start!)
     var pauseButton: SKLabelNode!  // 暫停按鈕
 
+    var gameOverTitle: SKLabelNode! // 顯示遊戲結束(Game Over!)
     var showScore: SKLabelNode!  // 遊戲結束時顯示分數
     var restartButton: SKLabelNode!  // 重新開始遊戲的按鈕
     var backButton: SKLabelNode!  // 返回主頁面的按鈕
@@ -42,6 +43,25 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var isInGame = false  // 檢查是否在遊戲內
     var isGamePaused = false  // 檢查是否暫停
 
+    func collisionBetween (bird: SKNode, object: SKNode) {
+        if object.name == "ground" {
+            isInGame = false
+            
+            gameScore.isHidden = true
+            pauseButton.isHidden = true
+            pauseButton.isPaused = true
+            
+            showScore.text = "Final Score:\(score)"
+            
+            gameOverTitle.isHidden = false
+            showScore.isHidden = false
+            restartButton.isHidden = false
+            restartButton.isPaused = false
+            backButton.isHidden = false
+            backButton.isPaused = false
+        }
+    }
+    
     func createCeiling() {
         ceiling = SKSpriteNode()
         ceiling.size = CGSize(width: 1534, height: 104)
@@ -51,7 +71,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         ceiling.name = "ceiling"
 
         ceiling.physicsBody = SKPhysicsBody(
-            rectangleOf: CGSize(width: 1534, height: 104))
+            rectangleOf: ceiling.size)
         ceiling.physicsBody?.isDynamic = false
     }
 
@@ -64,7 +84,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         ground.name = "ground"
 
         ground.physicsBody = SKPhysicsBody(
-            rectangleOf: CGSize(width: 1534, height: 104))
+            rectangleOf: ground.size)
         ground.physicsBody?.isDynamic = false
     }
 
@@ -129,9 +149,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         bird.name = "bird"
 
         bird.physicsBody = SKPhysicsBody(
-            rectangleOf: CGSize(width: 105, height: 90.75))
+            rectangleOf: bird.size)
         bird.physicsBody?.isDynamic = false
         bird.physicsBody?.allowsRotation = false
+        bird.physicsBody?.contactTestBitMask = bird.physicsBody?.collisionBitMask ?? UInt32.max
 
         bird.run(SKAction.repeatForever(SKAction.animate(withNormalTextures: birdTextureArray, timePerFrame: 0.1)))
     }
@@ -158,8 +179,66 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         addChild(pauseButton)
         pauseButton.name = "Pause"
     }
+    
+    func createGameOverTitle() {
+        gameOverTitle = SKLabelNode(fontNamed: "Impact")
+        gameOverTitle.text = "Game Over!"
+        gameOverTitle.fontColor = UIColor.black
+        gameOverTitle.horizontalAlignmentMode = .center
+        gameOverTitle.fontSize = 108
+        gameOverTitle.position = CGPoint(x: 384, y: 832)
+        gameOverTitle.zPosition = 1
+        addChild(gameOverTitle)
+    }
+    
+    func createShowScore() {
+        showScore = SKLabelNode(fontNamed: "Impact")
+        showScore.fontColor = UIColor.black
+        showScore.horizontalAlignmentMode = .center
+        showScore.fontSize = 72
+        showScore.position = CGPoint(x: 384, y: 744)
+        showScore.zPosition = 1
+        addChild(showScore)
+    }
+    
+    func createRestartButton() {
+        restartButton = SKLabelNode(fontNamed: "Impact")
+        restartButton.text = "Restart"
+        restartButton.fontColor = UIColor.black
+        restartButton.horizontalAlignmentMode = .center
+        restartButton.fontSize = 72
+        restartButton.position = CGPoint(x: 384, y: 512)
+        restartButton.zPosition = 1
+        addChild(restartButton)
+        restartButton.name = "Restart"
+    }
+    
+    func createBackButton() {
+        backButton = SKLabelNode(fontNamed: "Impact")
+        backButton.text = "Back to Menu"
+        backButton.fontColor = UIColor.black
+        backButton.horizontalAlignmentMode = .center
+        backButton.fontSize = 72
+        backButton.position = CGPoint(x: 384, y: 372)
+        backButton.zPosition = 1
+        addChild(backButton)
+        backButton.name = "Back"
+    }
 
+    func didBegin (_ contact: SKPhysicsContact ) {
+        guard let nodeA = contact.bodyA.node else { return }
+        guard let nodeB = contact.bodyB.node else { return }
+        if nodeA.name == "bird" {
+            collisionBetween (bird: nodeA, object: nodeB)
+        } else if nodeB.name == "bird" {
+            collisionBetween (bird: nodeB, object: nodeA)
+        }
+    }
+    
     override func didMove(to view: SKView) {
+        
+        physicsWorld.contactDelegate = self
+        
         self.anchorPoint = CGPoint(x: 0, y: 0)
         let sky = SKSpriteNode(imageNamed: "sky.png")
         sky.size = CGSize(width: 2048, height: 1024)
@@ -183,6 +262,17 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         createPauseButton()
         pauseButton.isHidden = true
         pauseButton.isPaused = true
+        
+        createGameOverTitle()
+        gameOverTitle.isHidden = true
+        createShowScore()
+        showScore.isHidden = true
+        createRestartButton()
+        restartButton.isHidden = true
+        restartButton.isPaused = true
+        createBackButton()
+        backButton.isHidden = true
+        backButton.isPaused = true
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -214,26 +304,61 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 hintTitle.isHidden = false
                 gameScore.isHidden = false
                 pauseButton.isHidden = false
-                pauseButton.isHidden = false
+                pauseButton.isPaused = false
 
             } else if node.name == "Score" {
 
             } else if node.name == "Back" {
                 isGameStarted = false
-
+                
+                bird.removeFromParent()
+                
+                gameOverTitle.isHidden = true
+                showScore.isHidden = true
+                restartButton.isHidden = true
+                restartButton.isPaused = true
+                backButton.isHidden = true
+                backButton.isPaused = true
+                
+                title.isHidden = false
+                startButton.isHidden = false
+                startButton.isPaused = false
+                scoreButton.isHidden = false
+                scoreButton.isPaused = false
+                
             } else if node.name == "Restart" {
                 isInGame = true
                 isGameStarted = false
-
+                
+                bird.removeFromParent()
+                
+                gameOverTitle.isHidden = true
+                showScore.isHidden = true
+                restartButton.isHidden = true
+                restartButton.isPaused = true
+                backButton.isHidden = true
+                backButton.isPaused = true
+                
+                createBird()
+                
+                hintTitle.isHidden = false
+                gameScore.isHidden = false
+                pauseButton.isHidden = false
+                pauseButton.isPaused = false
+                
             } else if node.name == "Pause" {
                 if isGamePaused == false {
                     pauseButton.text = "▸"
                     pauseButton.fontSize = 96
                     isGamePaused = true
+                    bird.physicsBody?.isDynamic = false
                 } else {
                     pauseButton.text = "⏸︎"
                     pauseButton.fontSize = 72
                     isGamePaused = false
+                    if isGameStarted == true {
+                        bird.physicsBody?.isDynamic = true
+                    }
                 }
             }
         }
@@ -245,7 +370,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 }
 
 struct GameView: View {
-    var scene: SKScene {
+    var scene: SKScene{
         let scene = GameScene(size: CGSize(width: 768, height: 1024))
         scene.scaleMode = .aspectFit
         return scene
